97a79c8c046f6fc2178f886eed534b3fa1d6b981
sarxaisa <118470659+sarxaisa@users.noreply.github.com>
2025-02-13 08:54:28 -0300

    Add files via upload

---
+++ portifolioTDS-main/script.js
@@ -0,0 +1,4 @@
+// a função console.log() serve para enviar mensagens por meio de console, esta é util para debugar o código
+console.log("Sou uma mensagem de console");
+// a função alert ela serve para exibir uma mensagem de alerta ao usuário
+alert("Sou uma mensagem de alerta");
